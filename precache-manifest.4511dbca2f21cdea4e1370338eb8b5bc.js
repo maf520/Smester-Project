a9411691b6d09511ec64039371a68805447f7236
maf520 <91660822+maf520@users.noreply.github.com>
2021-12-31 23:16:23 -0800

    Add files via upload

---
+++ precache-manifest.4511dbca2f21cdea4e1370338eb8b5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c69481f631df9a55ff6487ab4a8b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8356193293e4e242d026",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "f4988725a5e8371123fd",
+    "url": "/static/js/2.7ad4e3de.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.7ad4e3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8356193293e4e242d026",
+    "url": "/static/js/main.536bf107.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
